acb2172f70da79680a8c0d19511519b67a197d8f
llocretsa <parny.paul@gmail.com>
2022-04-18 11:00:39 -0300

    refactor: alterar a estratégia de teste para usar simulações de jest

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,36 +1,26 @@
-import { ILoadFacebookUserApi } from '@/data/contracts/apis'
 import { FacebookAuthenticationService } from '@/data/services'
 import { AuthenticationError } from '@/domain/errors'
-
-class LoadFacebookUserApiSpy implements ILoadFacebookUserApi {
-  token?: string
-  callsCount = 0
-  result: undefined
-
-  async loadUser(params: ILoadFacebookUserApi.Params): Promise<ILoadFacebookUserApi.Result> {
-    this.token = params.token
-    this.callsCount++
-    return this.result
-  }
-
-}
 
 describe('FacebookAuthenticationService', () => {
   // should call LoadFacebookUserApi with correct params
   it('deve chamar LoadFacebookUserApi com parâmetros corretos', async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
+    const loadFacebookUserApi = {
+      loadUser: jest.fn()
+    }
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
 
     await sut.perform({ token: 'any_token' })
 
-    expect(loadFacebookUserApi.token).toBe('any_token')
-    expect(loadFacebookUserApi.callsCount).toBe(1)
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   // should return AuthenticationError when LoadFacebookUserApi returns undefined
   it('deve retornar AuthenticationError quando LoadFacebookUserApi retornar undefined', async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
-    loadFacebookUserApi.result = undefined
+    const loadFacebookUserApi = {
+      loadUser: jest.fn()
+    }
+    loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
 
     const authResult = await sut.perform({ token: 'any_token' })
