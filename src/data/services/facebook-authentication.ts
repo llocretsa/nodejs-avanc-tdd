9e31a112a2da476fcfb4bd4d4dd295fcfde47e33
llocretsa <parny.paul@gmail.com>
2022-04-20 09:30:32 -0300

    feat: mover o login comercial para uma entidade de domínio

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -2,6 +2,7 @@ import { ILoadFacebookUserApi } from '@/data/contracts/apis'
 import { ILoadUserAccountRepository, ISaveFacebookAccountRepository } from '@/data/contracts/repos'
 import { AuthenticationError } from '@/domain/errors'
 import { IFacebookAuthentication } from '@/domain/feature'
+import { FacebookAccount } from '@/domain/models/facebook-account'
 
 export class FacebookAuthenticationService {
   constructor(
@@ -13,12 +14,9 @@ export class FacebookAuthenticationService {
     const fbData = await this.facebookApi.loadUser(params)
     if (fbData !== undefined) {
       const accountData = await this.userAccountRepo.load({ email: fbData.email })
-      await this.userAccountRepo.saveWithFacebook({
-        id: accountData?.id,
-        name: accountData?.name ?? fbData.name,
-        email: fbData.email,
-        facebookId: fbData.facebookId
-      })
+      const fbAccount = new FacebookAccount(fbData, accountData)
+
+      await this.userAccountRepo.saveWithFacebook(fbAccount)
     }
     return new AuthenticationError()
   }
