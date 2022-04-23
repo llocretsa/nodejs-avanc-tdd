37ceb958ae9b994102015c799959e2e5a9dde9fc
llocretsa <parny.paul@gmail.com>
2022-04-23 17:57:25 -0300

    test: deve obter as informações do usuário - FacebookApi

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -16,7 +16,9 @@ describe('FacebookApi', () => {
   })
 
   beforeEach(() => {
-    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' })
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -43,6 +45,19 @@ describe('FacebookApi', () => {
       params: {
         access_token: 'any_app_token',
         input_token: 'any_client_token'
+      }
+    })
+  })
+
+  // should get user info
+  it('deve obter as informações do usuário', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: {
+        fields: 'id,name,email',
+        access_token: 'any_client_token'
       }
     })
   })
