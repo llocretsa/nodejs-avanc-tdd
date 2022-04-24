ada996609fddd1e5ac6d6266569fde3eb9e02d73
llocretsa <parny.paul@gmail.com>
2022-04-24 13:41:03 -0300

    refactor: melhorar a qualidade do código

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -19,6 +19,7 @@ describe('FacebookApi', () => {
     httpClient.get
       .mockResolvedValueOnce({ access_token: 'any_app_token' })
       .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({ id: 'any_fb_id', name: 'any_fb_name', email: 'any_fb_email' })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -59,6 +60,17 @@ describe('FacebookApi', () => {
         fields: 'id,name,email',
         access_token: 'any_client_token'
       }
+    })
+  })
+
+  // should return facebook user info
+  it('deve retornar as informações do facebook para o usuário', async () => {
+    const fbUser = await sut.loadUser({ token: 'any_client_token' })
+
+    expect(fbUser).toEqual({
+      facebookId: 'any_fb_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email'
     })
   })
 })
