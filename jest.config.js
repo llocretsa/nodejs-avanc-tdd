b6c8532ff4cedf9645fcc41252360566fafbf1fd
llocretsa <parny.paul@gmail.com>
2022-04-24 14:00:37 -0300

    refactor: ignora arquivos index, e a camada main para a cobertura do jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 module.exports = {
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
   coverageDirectory: "coverage",
   coverageProvider: "babel",
   moduleNameMapper: {
