e237bff8498d8e03460735b44bcb50451b7e436b
llocretsa <parny.paul@gmail.com>
2022-04-26 14:12:35 -0300

    fix: fazer JwtTokenGenerator implementar TokenGenerator

--- src/infra/crypto/jwt-token-generator.ts
+++ src/infra/crypto/jwt-token-generator.ts
@@ -2,7 +2,7 @@ import { ITokenGenerator } from '@/data/contracts/crypto'
 
 import { sign } from 'jsonwebtoken'
 
-export class JwtTokenGenerator {
+export class JwtTokenGenerator implements ITokenGenerator {
   constructor(private readonly secret: string) { }
 
   async generateToken(params: ITokenGenerator.Params): Promise<ITokenGenerator.Result> {
