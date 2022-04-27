00efd7aaee4c87f5d8a43c9bc1fa03f5140ea1b8
llocretsa <parny.paul@gmail.com>
2022-04-27 17:31:28 -0300

    refactor: movido código de produção para o seu próprio arquivo

---
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -0,0 +1,56 @@
+import { PgUser } from '@/infra/postgres/entities'
+import { PgUserAccountRepository } from '@/infra/postgres/repos'
+
+import { IBackup, IMemoryDb, newDb } from 'pg-mem'
+import { getConnection, getRepository, Repository } from 'typeorm'
+
+const makeFakeDb = async (entities?: any[]): Promise<IMemoryDb> => {
+  const db = newDb()
+  const connection = await db.adapters.createTypeormConnection({
+    type: 'postgres',
+    entities: entities ?? ['src/infra/postgres/entities/index.ts']
+  })
+  await connection.synchronize()
+  return db
+}
+
+describe('PgUserAccountRepository', () => {
+  describe('load', () => {
+    let sut: PgUserAccountRepository
+    let pgUserRepo: Repository<PgUser>
+    let backup: IBackup
+
+    beforeAll(async () => {
+      const db = await makeFakeDb([PgUser])
+      backup = db.backup()
+      pgUserRepo = getRepository(PgUser)
+    })
+
+    afterAll(async () => {
+      await getConnection().close()
+    })
+
+    beforeEach(() => {
+      backup.restore()
+      sut = new PgUserAccountRepository()
+    })
+
+    // should return an account if email exists
+    it('deve retornar uma conta se o email existir', async () => {
+      await pgUserRepo.save({ email: 'any_email' })
+
+      const account = await sut.load({ email: 'any_email' })
+
+      expect(account).toEqual({ id: '1' })
+    })
+
+    // should return undefined if email does not exists
+    it('deve retornar undefined se o email não existir', async () => {
+      const sut = new PgUserAccountRepository()
+
+      const account = await sut.load({ email: 'any_email' })
+
+      expect(account).toBeUndefined()
+    })
+  })
+})
