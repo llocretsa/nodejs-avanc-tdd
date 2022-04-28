7ca512cad8b2cf3610ad366f8c54536b7a29cf1b
llocretsa <parny.paul@gmail.com>
2022-04-28 11:42:26 -0300

    test: garantir que PgUserAccountRepository retorne o id da conta

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -46,7 +46,7 @@ describe('PgUserAccountRepository', () => {
   describe('saveWithFacebook', () => {
     // should create an account if id is undefined
     it('deve criar uma conta se o id é undefined', async () => {
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         email: 'any_email',
         name: 'any_name',
         facebookId: 'any_fb_id'
@@ -54,6 +54,7 @@ describe('PgUserAccountRepository', () => {
       const pgUser = await pgUserRepo.findOne({ email: 'any_email' })
 
       expect(pgUser?.id).toBe(1)
+      expect(id).toBe('1')
     })
 
     // should update account if id is undefined
@@ -64,7 +65,7 @@ describe('PgUserAccountRepository', () => {
         facebookId: 'any_fb_id'
       })
 
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         id: '1',
         email: 'new_email',
         name: 'new_name',
@@ -78,6 +79,7 @@ describe('PgUserAccountRepository', () => {
         name: 'new_name',
         facebookId: 'new_fb_id'
       })
+      expect(id).toBe('1')
     })
   })
 })
