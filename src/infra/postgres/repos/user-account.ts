e33bb7afb5bbe7820c5dd59f27ad60d1af4d11d0
llocretsa <parny.paul@gmail.com>
2022-04-28 11:48:20 -0300

    refactor: em PgUserAccountRepository deve fazer a implementação da interface ISaveFacebookAccountRepository

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -8,7 +8,7 @@ type LoadResult = ILoadUserAccountRepository.Result
 type SaveParams = ISaveFacebookAccountRepository.Params
 type SaveResult = ISaveFacebookAccountRepository.Result
 
-export class PgUserAccountRepository implements ILoadUserAccountRepository {
+export class PgUserAccountRepository implements ILoadUserAccountRepository, ISaveFacebookAccountRepository {
   private readonly pgUserRepo = getRepository(PgUser)
 
   async load(params: LoadParams): Promise<LoadResult> {
